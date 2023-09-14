77334326d7f31d59215c2b1d4651c197ebf5cf0f
fadoua <fadouaahachmi0220@gmail.com>
2023-09-14 18:56:25 +0100

    challenge5

---
+++ challenge5CN.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int NA, mois, semaine, jour, heure;
+    printf("Svp entrez le Nombre d'années :");
+    scanf("%d",&NA);
+    mois = 30 * NA;
+    semaine = 4 * 30 * NA;
+    jour = 7 * 4 * 30 * NA;
+    heure =24 * 7 * 4 * 30 * NA;
+    printf("mois %d\nsemaine %d\njour %d\nheure %d\n",mois,semaine,jour,heure);
+
+    return 0;
+}
