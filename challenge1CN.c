b45336d96b9451aaeb56d031f5b643845d8e1817
fadoua <fadouaahachmi0220@gmail.com>
2023-09-14 16:59:23 +0100

    challenge1

---
+++ challenge1CN.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int N;
+    printf("entrez un nombre :");
+    scanf("%d",&N);
+    if ( N < 0 )
+    printf("ce nombre est impaire.");
+    else
+    printf("ce nombre est paire.");
+
+
+    return 0;
+}
