9ad12a94f8349b57b621570665ccca15d07b23af
fadoua <fadouaahachmi0220@gmail.com>
2023-09-14 10:13:31 +0100

    challenge2

---
+++ challenge3.c
@@ -0,0 +1,27 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    float A,B,SM,ST,ML,DV;
+    printf("entrer le nombre A :");
+    scanf("%f",&A);
+    printf("entrer le nombre B :");
+    scanf("%f",&B);
+    SM = A + B;
+    ST = A - B;
+    ML = A * B;
+    DV = A / B;
+    printf("la somme c'est : %.2f\n",SM);
+    printf(" la soustraction c'est: %.2f\n",ST);
+    printf("la Multiplication c'est : %.2f\n",ML);
+    printf("la Division c'est : %.2f\n",DV);
+    
+    
+    
+    
+
+    return 0;
+}
+
+
+
