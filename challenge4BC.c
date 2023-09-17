1d81f7deed7fc283f6842fa28d645551db43346f
fadoua <fadouaahachmi0220@gmail.com>
2023-09-17 16:37:14 +0100

    challenge_4_boucle

---
+++ challenge4BC.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+  int N,i, S;
+  printf("entrez un nombre : \n");
+  scanf("%d",&N);
+  for(i=N;i<=100;i++)
+    printf("%d\n",i);
+  S = S + N ;
+  printf("la somme c'est : %d",S);
+
+
+    return 0;
+}
