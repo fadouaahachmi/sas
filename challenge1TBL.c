b54f914d2992fa1b54832ea2d86ba0b40ed42404
fadoua <fadouaahachmi0220@gmail.com>
2023-09-19 11:56:04 +0100

    challenge_1_Tableaux

---
+++ challenge1TBL.c
@@ -0,0 +1,23 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+  int T[10],i;
+  for(i=1;i<=10;i++){
+  T[i]= i;
+  T[i]= i;
+  T[i]= i;
+  T[i]= i;
+  T[i]= i;
+  T[i]= i;
+  T[i]= i;
+  T[i]= i;
+  T[i]= i;
+  T[i]= i;
+  printf("%d,",T[i]);
+
+  }
+
+
+    return 0;
+}
