d7b2e33fb11261af4d0b931d28d83baf06b3479a
fadoua <fadouaahachmi0220@gmail.com>
2023-09-19 13:56:20 +0100

    challenge_2_Tableaux

---
+++ challenge2TBL.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int n[3],i,min,max;
+
+    for(i=0;i<3;i++){
+        printf("Entrez N%d :",i+1);
+        scanf("%d",&n[i]);
+        if(n[i]<min){
+            min=n[i];
+        }
+        if(n[i]>max){
+            max=n[i];
+        }
+    }
+    printf("Le min est : %d\n",min);
+    printf("le max est : %d\n",max);
+
+    return 0;
+}
